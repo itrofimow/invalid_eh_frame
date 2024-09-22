a239a3bbc44d8071455b6336433a45104d5297a1
Ivan Trofimov <i.trofimow@yandex.ru>
2024-09-23 01:06:42 +0300

    reproduces

---
+++ throw_test.cpp
@@ -0,0 +1,26 @@
+void DoWork() {
+		try {
+			throw 42;
+		} catch (...) {
+				throw;
+		}
+}
+
+int TryDoWork() {
+	int err = 0;
+
+	try {
+		DoWork();
+	} catch (const int& ex) {
+		err = ex;
+	} catch (...) {
+		err = 1;
+	}
+
+	return err;
+}
+
+int main() {
+	const auto err = TryDoWork();
+	return err;
+}
